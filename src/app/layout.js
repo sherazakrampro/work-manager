382f2b7445b1a495219923270acb3115ae91b684
Sheraz Akram <sherazakram.pro@gmail.com>
2023-09-03 17:14:09 +0500

    Footer Created

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,8 +13,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        <div className="flex flex-col h-screen justify-between">
+          <Navbar />
+          <div className="mb-auto">{children}</div>
+          <Footer />
+        </div>
       </body>
     </html>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="h-20 bg-blue-600 flex flex-col justify-center items-center">
+      <div className="text-lg">Coded by: Sheraz Akram</div>
+      <div className="text-lg">+92-344-6245730</div>
+    </footer>
+  );
+};
+
+export default Footer;
