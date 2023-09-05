76459454452d3089a03b648d32da7ae70998de93
Sheraz Akram <sherazakram.pro@gmail.com>
2023-09-05 21:32:46 +0500

    task APIs

--- src/app/api/tasks/[taskId]/route.js
+++ src/app/api/tasks/[taskId]/route.js
@@ -16,7 +16,49 @@ export async function GET(request, { params }) {
 }
 
 // Update a Task by ID
-export async function PUT(request, { params }) {}
+export async function PUT(request, { params }) {
+  try {
+    const { taskId } = params;
+
+    const { title, content, status } = await request.json();
+
+    let task = await Task.findById(taskId);
+
+    task.title = title;
+    task.content = content;
+    task.status = status;
+
+    const updatedTask = await task.save();
+
+    return NextResponse.json(updatedTask);
+  } catch (error) {
+    console.log(error);
+
+    return getResponseMessage("Error in updating task", false, 500);
+  }
+}
 
 // Delete a Task by ID
-export async function DELETE(request, { params }) {}
+export async function DELETE(request, { params }) {
+  try {
+    const { taskId } = params;
+
+    await Task.deleteOne({
+      _id: taskId,
+    });
+
+    return NextResponse.json(
+      {
+        message: "Task deleted successfully",
+        success: true,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+
+    return getResponseMessage("Error in task deletion", false, 500);
+  }
+}
